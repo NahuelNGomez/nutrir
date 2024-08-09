5f1a8791138bbbeaec9ef3ffd75f4d7fc50559ce
NahuelNGomez <nngomez@fi.uba.ar>
2024-08-09 13:20:11 -0300

    fix import csv

--- django_nutrir-develop/departamento/management/commands/import_departamento.py
+++ django_nutrir-develop/departamento/management/commands/import_departamento.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         path = kwargs['path']
-        with open(path, 'rt') as f:
+        with open(path, 'rt', encoding='utf-8') as f:
             reader = csv.reader(f, delimiter=';')
             for row in reader:
                 #import pdb
