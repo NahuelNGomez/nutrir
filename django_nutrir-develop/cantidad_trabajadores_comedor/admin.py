b0987e3de554474ac136307d0489582993e0d56c
Jose Luis Juan <jjarro@fi.uba.ar>
2024-09-01 01:11:37 -0300

    sorted 'cantidad_trabajadores'

--- django_nutrir-develop/cantidad_trabajadores_comedor/admin.py
+++ django_nutrir-develop/cantidad_trabajadores_comedor/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import CantidadTrabajadoresComedor
+from django.db.models.functions import Cast, Substr
+from utils.functions import Position, IntegerField
 
 # Register your models here.
 class CantidadTrabajadoresComedorAdmin(admin.ModelAdmin):
 
     list_display = ['rango']
     search_fields = ('rango',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        posicion_guion = Position('rango', expression="'-'")
+
+        return qs.annotate(
+            valor_inicial=Cast(Substr('rango', 1, posicion_guion-1), IntegerField())
+        ).order_by('valor_inicial')
 
 admin.site.register(CantidadTrabajadoresComedor, CantidadTrabajadoresComedorAdmin)
