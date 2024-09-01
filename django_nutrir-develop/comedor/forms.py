c91aac2999ee4568e07c85f62909b5ebc6a064db
Jose Luis Juan <jjarro@fi.uba.ar>
2024-09-01 01:29:56 -0300

    sorted tables in 'comedor' form

--- django_nutrir-develop/comedor/forms.py
+++ django_nutrir-develop/comedor/forms.py
@@ -4,34 +4,48 @@ from provincia.models import Provincia
 from departamento.models import Departamento
 from gobierno_local.models import GobiernoLocal
 from localidad.models import Localidad
-from .models import Comedor
+from .models import Comedor, AsistentesDiariosComedor, CantidadTrabajadoresComedor
+from utils.functions import Position, IntegerField
+from django.db.models.functions import Cast, Substr
 
 
 class ComedorAdminForm(forms.ModelForm):
-		"""Form para que en comedor se pueda buscar las localidades, los gobiernos locales
-		 y los departamenos en cascada"""
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		posicion_guion = Position('rango', expression="'-'")
+		
+		self.fields['asistentes_diarios'].queryset = AsistentesDiariosComedor.objects.annotate(
+			valor_inicial=Cast(Substr('rango', 1, posicion_guion-1), IntegerField())
+        ).order_by('valor_inicial')
 
-		provincia = forms.ModelChoiceField(
-			queryset=Provincia.objects.all(),
-			widget=autocomplete.ModelSelect2(url='provincia-autocomplete')
-		)
-		departamento = forms.ModelChoiceField(
-			queryset=Departamento.objects.all(),
-			widget=autocomplete.ModelSelect2(url='departamento-autocomplete', forward=('provincia',)),
-			blank=True,
-			required=False
-		)
-		gobierno_local = forms.ModelChoiceField(
-			queryset=GobiernoLocal.objects.all(),
-			widget=autocomplete.ModelSelect2(url='gobiernoLocal-autocomplete', forward=('departamento',)),
-			blank=True,
-			required=False
-		)
-		localidad = forms.ModelChoiceField(
-			queryset=Localidad.objects.all(),
-			widget=autocomplete.ModelSelect2(url='localidad-autocomplete', forward=('gobierno_local',)),
-			blank=True,
-			required=False
-		)
+		self.fields['cantidad_trabajadores'].queryset = CantidadTrabajadoresComedor.objects.annotate(
+            valor_inicial=Cast(Substr('rango', 1, posicion_guion-1), IntegerField())
+        ).order_by('valor_inicial')
+
+	"""Form para que en comedor se pueda buscar las localidades, los gobiernos locales
+		y los departamenos en cascada"""
+
+	provincia = forms.ModelChoiceField(
+		queryset=Provincia.objects.all(),
+		widget=autocomplete.ModelSelect2(url='provincia-autocomplete')
+	)
+	departamento = forms.ModelChoiceField(
+		queryset=Departamento.objects.all(),
+		widget=autocomplete.ModelSelect2(url='departamento-autocomplete', forward=('provincia',)),
+		blank=True,
+		required=False
+	)
+	gobierno_local = forms.ModelChoiceField(
+		queryset=GobiernoLocal.objects.all(),
+		widget=autocomplete.ModelSelect2(url='gobiernoLocal-autocomplete', forward=('departamento',)),
+		blank=True,
+		required=False
+	)
+	localidad = forms.ModelChoiceField(
+		queryset=Localidad.objects.all(),
+		widget=autocomplete.ModelSelect2(url='localidad-autocomplete', forward=('gobierno_local',)),
+		blank=True,
+		required=False
+	)
 
 
