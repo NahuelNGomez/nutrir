15a1bedd09dfcd7d5cbea48b9832fc6512905f50
Ignacio Vetrano <79949866+IVetrano@users.noreply.github.com>
2024-09-02 13:55:39 -0300

    Ahora se puede seleccionar unidad al añadir alimento
    Ahora se puede seleccionar entre Gramo o Kilogramo para la cantidad de referencia, y se convierte a gramos en caso de elegir kilogramos.
    
    ADVERTENCIA: Para actualizar el modelo de Alimento ejecutar:
    
    python manage.py makemigrations
    python manage.py migrate

--- django_nutrir-develop/alimento/models.py
+++ django_nutrir-develop/alimento/models.py
@@ -6,9 +6,15 @@ from django.db import models
 
 class Alimento(models.Model):
 
+	UNIDAD_CHOICES = [
+		("g", "g"),
+		("Kg", "Kg")
+	]
+
 	nombre = models.CharField("Nombre", max_length=100, unique=True)
 	foto = models.ImageField(upload_to='images')
 	cantidad_porcion = models.DecimalField("Cantidad de Referencia", null=False, blank=False, decimal_places=2, max_digits=16)
+	unidades = models.CharField("Unidad", max_length=10, choices=UNIDAD_CHOICES, default='g')
 	hidratos_carbono = models.DecimalField("Hidratos de Carbono", null=False, blank=False, decimal_places=2, max_digits=16, help_text="En gramos")
 	proteinas = models.DecimalField("Proteinas", null=False, blank=False, decimal_places=2, max_digits=16, help_text="En gramos")
 	grasas = models.DecimalField("Grasas Saturadas", null=False, blank=False, decimal_places=2, max_digits=16, help_text="En gramos")
@@ -16,9 +22,15 @@ class Alimento(models.Model):
 	energia = models.DecimalField("Kilocalorias", null=False, blank=False, decimal_places=2, max_digits=16)
 	sodio = models.DecimalField("Sodio", null=False, blank=False, decimal_places=2, max_digits=16, help_text="En gramos")
 
+	def save(self, *args, **kwargs):
+		if self.unidades == 'Kg':
+			self.cantidad_porcion = self.cantidad_porcion * 1000
+		
+		super().save(*args, **kwargs)
 
 	def __str__(self):
 		return f"{self.nombre}"
 
 	class Meta:
 		verbose_name_plural = "Alimentos"
+
