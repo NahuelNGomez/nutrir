9e7eeda393ac83f843d3a707da18d62028cf797b
Ignacio Ezequiel Vetrano <ivetrano@fi.uba.ar>
2024-09-16 16:15:56 -0300

    Ahora se pueden agregar unidades a los alimentos registrados en las encuestas. Ademas se pueden crear nuevas unidades en la barra de navegacion de la izquierda.

--- django_nutrir-develop/alimento/urls.py
+++ django_nutrir-develop/alimento/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
      re_path('$', views.AlimentoViewList.as_view(), name="alimento"),
-
+     re_path('$', views.UnidadesViewList.as_view(), name="unidad"),
 ]
